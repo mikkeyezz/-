7d58ad0b1aa66475cd3c4e0d82d592322a67d237
mikkeyezz <mesipiv1212@gmail.com>
2025-09-18 19:55:39 +0300

    Create java.java

---
+++ java.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        
+        myList.add("Apple");
+        myList.add("Banana");
+        myList.add("Cherry");
+        
+        System.out.println("Список: " + myList);
+    }
+}
